59000edb5a4f8fefc9431913dfea6c6e6c102464
Neko <neko@nomblocks.io>
2022-10-22 03:47:49 +0000

    Use password file and create table on init

---
+++ docker_postgres_init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.validator_stats (
+    run_time timestamptz NOT NULL,
+    block_number int NOT NULL,
+    moniker text NULL,
+    address text NOT NULL,
+    num_delegators int NULL,
+    pc numeric NULL,
+    total numeric NULL
+);
