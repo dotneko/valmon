6390ccde15244a0a826f0053dbc044a25715ff06
Neko <neko@nomblocks.io>
2022-10-23 03:02:11 +0000

    Use async for main get_statistics and timer

--- monitor/daemon.py
+++ monitor/daemon.py
@@ -1,21 +1,22 @@
+import asyncio
 import logging
 import sys
-
+import time
 from datetime import datetime, timezone
-from time import sleep
+from itertools import repeat
 
+from pyibc_async import get_validator_stats_async
 from pyibc_chain.queries import get_latest_block_height
 from pyibc_chain.validators import get_latest_validator_set_sorted
-from pyibc_modified import get_validator_stats
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 from utils import get_config
 
 # Setup logging
 logging.basicConfig(
-    format = "%(levelname)s | %(asctime)s | %(message)s",
-    stream = sys.stdout,
-    level = logging.INFO,
+    format="%(levelname)s | %(asctime)s | %(message)s",
+    stream=sys.stdout,
+    level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
 
@@ -26,22 +27,34 @@ def update_valset() -> (int, dict):
     return (valset_blocknumber, validators)
 
 
-def update_statistics(engine, validators: dict) -> dict:
+async def update_statistics(engine, validators: dict) -> dict:
     """Get latest validator set"""
     stats = {}
     run_time: datetime = datetime.now(timezone.utc)
     block_number: int = get_latest_block_height(ONOMY_REST)
     total_token_share: int = 0
-    for idx, op_addr in enumerate(validators):
+    val_addrs = [validator for validator in validators.keys()]
+    start_time: time = time.time()
+    logging.info(f"Requesting data at block {block_number}")
+    resps = await asyncio.gather(
+        *map(
+            get_validator_stats_async,
+            repeat(CHAIN),
+            repeat(ONOMY_REST),
+            val_addrs,
+            repeat(True),  # include_number_of_unique_delegations=True
+        )
+    )
+    logging.info(f"Elapsed time: {time.time() - start_time}s")
+    for resp in resps:
+        op_addr = resp["operator_address"]
         stats[op_addr] = {}
-        stats[op_addr]["moniker"]: str = validators[op_addr]["moniker"]
+        stats[op_addr]["moniker"]: str = resp["moniker"]
         # Get validator unique delegators
-        logger.info(f"Retrieving delegator stats for {stats[op_addr]['moniker']}")
-        detailed: dict = get_validator_stats(CHAIN, ONOMY_REST, op_addr, True)
-        stats[op_addr]["num_delegators"]: int = detailed["unique_delegators"]
-        stats[op_addr]["bonded_utokens"]: int = detailed["bonded_utokens"]
-        stats[op_addr]["bonded_tokens"]: str = detailed["bonded_tokens"]
-        total_token_share += int(detailed["bonded_utokens"])
+        stats[op_addr]["num_delegators"]: int = resp["unique_delegators"]
+        stats[op_addr]["bonded_utokens"]: int = resp["bonded_utokens"]
+        stats[op_addr]["bonded_tokens"]: str = resp["bonded_tokens"]
+        total_token_share += int(resp["bonded_utokens"])
 
     for op_addr in validators:
         stats[op_addr]["pc"]: float = (
@@ -56,17 +69,17 @@ def update_statistics(engine, validators: dict) -> dict:
             "pc": stats[op_addr]["pc"],
             "total": stats[op_addr]["bonded_utokens"],
         }
-        logger.info(
-            "{} {} {:15} {:>6} {}% {}".format(
-                run_time,
-                block_number,
-                stats[op_addr]["moniker"],
-                stats[op_addr]["num_delegators"],
-                stats[op_addr]["pc"],
-                # stats[op_addr]["bonded_utokens"],
-                stats[op_addr]["bonded_tokens"],
-            )
-        )
+        #        logger.info(
+        #            "{} {} {:15} {:>6} {}% {}".format(
+        #                run_time,
+        #                block_number,
+        #                stats[op_addr]["moniker"],
+        #                stats[op_addr]["num_delegators"],
+        #                stats[op_addr]["pc"],
+        #                # stats[op_addr]["bonded_utokens"],
+        #                stats[op_addr]["bonded_tokens"],
+        #            )
+        #        )
         insert = text(
             """INSERT INTO validator_stats (run_time, block_number, moniker, address, num_delegators, pc, total)
                      VALUES
@@ -77,7 +90,20 @@ def update_statistics(engine, validators: dict) -> dict:
             with con.begin():
                 con.execute(insert, data)
                 con.commit()
+    logger.info("Wrote to database")
     return stats
+
+
+async def interval_statistics(engine, interval):
+    validators = {}
+    (valset_blocknumber, validators) = update_valset()
+    logger.info(
+        f"{len(validators)} active validators at block height {valset_blocknumber} [Poll interval: {interval}s]"
+    )
+    if len(validators) > 0:
+        await asyncio.gather(
+            update_statistics(engine, validators), asyncio.sleep(interval)
+        )
 
 
 if __name__ == "__main__":
@@ -87,19 +113,14 @@ if __name__ == "__main__":
     ONOMY_REST: str = get_config("rest_endpoint")
     WAIT: int = get_config("poll_interval")
     PG: dict = get_config("pg_settings")
-    PG_DBPATH: str = f"{PG['username']}:{PG['password']}@{PG['host']}:{PG['port']}/{PG['dbname']}"
+    PG_DBPATH: str = (
+        f"{PG['username']}:{PG['password']}@{PG['host']}:{PG['port']}/{PG['dbname']}"
+    )
 
-    validators = {}
-    valset_blocknumber: int = None
-
-    (valset_blocknumber, validators) = update_valset()
-    logger.info(f"Got {len(validators)} active validators at block height {valset_blocknumber}")
     engine = create_engine(
         "postgresql+psycopg2://" + PG_DBPATH,
         # execution_options={"isolation_level": "AUTOCOMMIT"},
         future=True,
     )
     while True:
-        stats = update_statistics(engine, validators)
-        logger.info(f"Waiting for {WAIT}s")
-        sleep(WAIT)
+        stats = asyncio.run(interval_statistics(engine, WAIT))
